0799e589403d8beb59f46072421f581e52e67074
Julio Ribeiro <juliomobdeveloper@gmail.com>
2018-01-02 13:25:02 -0200

    Addes some urls

--- app/src/main/java/shido/com/swordmaster/di/ApplicationComponent.kt
+++ app/src/main/java/shido/com/swordmaster/di/ApplicationComponent.kt
@@ -14,6 +14,9 @@ import javax.inject.Singleton
 @Component(modules = [ApplicationModule::class, RoomModule::class]) //Modules Required to satify all the dependencies
 interface ApplicationComponent {
 
+    //https://antonioleiva.com/dagger-android-kotlin/
+    //https://github.com/satorufujiwara/kotlin-architecture-components/tree/master/app/src/main/java/jp/satorufujiwara/kotlin/di
+    //https://medium.com/@burakeregar/kotlin-in-android-mvp-dependency-injection-dagger2-clean-architecture-13a47869dba4
     fun inject(listFragment: ListFragment)
     fun inject(createFragment: CreateFragment)
     fun inject(detailFragment: DetailFragment)
